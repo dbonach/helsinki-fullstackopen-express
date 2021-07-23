1b4a186128aaba3eb6dedb7cd758ec0b249f5d2b
dbonach <deivitlopes@gmail.com>
2021-07-22 23:25:46 -0300

    Finish ex-3.13
    Updade index.js to fetch data from the database
    
    The data is fetched from a MongoDB through the
    /models.contact.js module.

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
-const app = express()
 const cors = require('cors')
 let morgan = require('morgan')
+require('dotenv').config()
+const Contact = require('./models/contact')
+const app = express()
 
 app.use(express.json())
 app.use(cors())
@@ -14,84 +16,62 @@ morgan.token('data', function (req, res) {
 // app.use(morgan('tiny'))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
 
-
-let phonebook = [
-  {
-    "id": 1,
-    "name": "Arto Hellas",
-    "number": "040-123456"
-  },
-  {
-    "id": 2,
-    "name": "Ada Lovelace",
-    "number": "39-44-5323523"
-  },
-  {
-    "id": 3,
-    "name": "Dan Abramov",
-    "number": "12-43-234345"
-  },
-  {
-    "id": 4,
-    "name": "Mary Poppendieck",
-    "number": "39-23-6423122"
-  }
-]
-
 app.get('/api/persons', (req, res) => {
-  res.json(phonebook)
+  Contact.find({}).then(result => {
+    res.json(result)
+  })
 })
 
-app.post('/api/persons', (req, res) => {
-  const body = req.body
+// app.post('/api/persons', (req, res) => {
+//   const body = req.body
 
-  if (!body.name && !body.number) {
-    return res.status(400).json({
-      error: 'Request should contain name and number!'
-    })
-  }
+//   if (!body.name && !body.number) {
+//     return res.status(400).json({
+//       error: 'Request should contain name and number!'
+//     })
+//   }
 
-  if (phonebook.find(p => p.name === body.name)) {
-    return res.status(400).json({
-      error: 'name must be unique'
-    })
-  }
+//   if (phonebook.find(p => p.name === body.name)) {
+//     return res.status(400).json({
+//       error: 'name must be unique'
+//     })
+//   }
 
-  const contact = {
-    id: parseInt(Math.random() * 1000),
-    name: body.name,
-    number: body.number,
-  }
+//   const contact = {
+//     id: parseInt(Math.random() * 1000),
+//     name: body.name,
+//     number: body.number,
+//   }
 
-  phonebook = phonebook.concat(contact)
+//   phonebook = phonebook.concat(contact)
 
-  res.json(contact)
-})
+//   res.json(contact)
+// })
 
-app.get('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id)
-  const contact = phonebook.filter((c) => c.id === id)
+// app.get('/api/persons/:id', (req, res) => {
+//   const id = Number(req.params.id)
+//   const contact = phonebook.filter((c) => c.id === id)
 
-  if (contact.length === 0) {
-    return res.status(404).send(`The person with id ${id} doesn't exist in the server.`)
-  }
+//   if (contact.length === 0) {
+//     return res.status(404).send(`The person with id ${id} doesn't exist in the server.`)
+//   }
 
-  res.json(contact)
-})
+//   res.json(contact)
+// })
 
-app.delete('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id)
+// app.delete('/api/persons/:id', (req, res) => {
+//   const id = Number(req.params.id)
 
-  phonebook = phonebook.filter(c => c.id !== id)
-  res.status(204).end()
-})
+//   phonebook = phonebook.filter(c => c.id !== id)
+//   res.status(204).end()
+// })
 
-app.get('/info', (req, res) => {
-  const msg = `Phonebook has info for ${phonebook.length} people`
-    + '<br/><br/>' + `${new Date}`;
+// app.get('/info', (req, res) => {
+//   const msg = `Phonebook has info for ${phonebook.length} people`
+//     + '<br/><br/>' + `${new Date}`;
 
-  res.send(msg)
-})
+//   res.send(msg)
+// })
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
